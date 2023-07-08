574abc02a6afacf62daf90af1bf827161319050d
brucul <afrizaf3@gmail.com>
2023-07-08 11:51:28 +0700

    update view

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -6,7 +6,7 @@
         <div class="hero text-white hero-bg-image hero-bg-parallax" style="background-image: url('assets/img/unsplash/andre-benz-1214056-unsplash.jpg');">
             <div class="hero-inner">
                 <h2>Hallo!</h2>
-                <p class="lead">Selamat datang di Sistem PPDB online SD Negeri 49 Kota Parepare.</p>
+                <p class="lead">Selamat datang di Sistem PPDB online {{ getSetting()->school_name }}.</p>
                 <p style="font-size: 80%">Pendaftaran dibuka tanggal <b class="text-info">{{getSetting()->start_date->format('d F Y')}}</b> sampai <b class="text-info">{{getSetting()->end_date->format('d F Y')}}</b>.</p>
                 <div class="mt-4">
                     <a href="{{ route('fe.instruction') }}" class="btn btn-outline-white btn-lg btn-icon icon-left"><i class="fas fa-circle-info"></i> Lihat Petunjuk Pendaftaran</a>
